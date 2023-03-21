2ad103f2bfa25a22ffa4c569ab4b565723c35078
Hümeyra Polat <71139790+humeyrapolat@users.noreply.github.com>
2023-03-21 22:01:08 +0300

    second commit
    Co-Authored-By: Hümeyra Köseoğlu <71442681+humeyrakoseoglu@users.noreply.github.com>

---
+++ lib/util/consts.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const backgGroundColor = Color.fromARGB(0, 0, 0, 0);
+const pinkColor = Color.fromARGB(255, 173, 8, 91);
+
+const black = Colors.black;
+const primaryColor = Colors.white;
+const secondaryColor = Colors.grey;
+const darkGreyColor = Color.fromARGB(97, 97, 97, 1);
+
+Widget sizeVertical(double height) {
+  return SizedBox(height: height);
+}
+
+Widget sizeHorizontal(double width) {
+  return SizedBox(width: width);
+}
